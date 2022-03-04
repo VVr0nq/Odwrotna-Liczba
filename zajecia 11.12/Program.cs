2072cc4e8c1778bd7c2a26889f4e82b2b9cd5565
VVr0nq <arek.migocki@gmail.com>
2022-03-04 21:45:00 +0100

    Add project files.

---
+++ zajecia 11.12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zajecia_11._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int n, odwrotna = 0, a;
+                Console.Write("Podaj liczbe ");
+                n = int.Parse(Console.ReadLine());
+                while (n != 0)
+                {
+                    a = n % 10;
+                    odwrotna = odwrotna * 10 + a;
+                    n /= 10;
+                }
+                Console.Write("Odwrotna Liczba: " + odwrotna);
+
+            } 
+        }
+    }
+}
